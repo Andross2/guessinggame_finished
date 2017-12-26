c088f25d0b78df79f83a26a6553529cdd5fe72d0
Andrew <31263356+Andross2@users.noreply.github.com>
2017-12-26 13:59:39 -0500

    shell game finished

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,28 @@
-guessinggame file finished
+#guessinggame file finished
+
+clear
+echo "Please guess the number of files in the working directory!"
+echo "Your guess: "
+read guess
+
+function get_file_number {
+	local number=$(ls -l | wc -l)-1
+	echo $number
+}
+
+correct_num=$(get_file_number)
+
+while [[ $guess -ne $correct_num ]]
+do
+	if [[ $guess -gt $correct_num ]]
+	then
+		echo "Sorry, your guess was too high..."
+	else 
+		echo "Sorry, your guess was too low..."
+	fi
+	echo
+	echo "Guess again: "
+	read guess
+done
+
+echo "Congratulations you guessed the right number."
